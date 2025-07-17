73faf52d60633dc59e7e2780434989daaae2713f
Colin Garner <colin.robert.garner@gmail.com>
2025-07-17 18:38:45 -0500

    Add temporary admin promotion endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
   root "home#index"
   get "home/index"
   
+  # Temporary admin promotion route (remove after use)
+  get "promote_me_to_admin", to: "application#promote_to_admin"
+  
   # Admin routes - protected by authentication in controller
   namespace :admin do
     root "dashboard#index"
